e061335f7febcbb1373d9c2241f468a223b80643
minjun <ijmtylhtf@naver.com>
2023-05-10 22:02:30 +0900

    [Add] #2 - 패킷 캡쳐 service 추가

--- src/main/java/com/example/domain/child/service/ChildService.java
+++ src/main/java/com/example/domain/child/service/ChildService.java
@@ -5,17 +5,24 @@ import com.example.domain.child.entity.Child;
 import com.example.domain.child.repository.ChildRepository;
 import com.example.domain.parent.entity.Parent;
 import com.example.domain.parent.repository.ParentRepository;
+import com.example.global.JPacketCapture;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.io.IOException;
 
 @Service
 @RequiredArgsConstructor
 public class ChildService {
     private final ChildRepository childRepository;
-    private final ParentRepository parentRepository;
+    private final JPacketCapture jPacketCapture;
     public String createUser(PostChildReq postUserReq){
         Child child = postUserReq.toEntity();
         childRepository.save(child);
         return "success";
+    }
+
+    public void startCapturePacket(Long userIdx) throws IOException {
+        jPacketCapture.startCaptureByInterface(userIdx);
     }
 }
