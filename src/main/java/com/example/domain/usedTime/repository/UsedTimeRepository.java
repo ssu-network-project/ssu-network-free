92b52187c889a0ab4e8219ec5e0dc927a7fd0462
minjun <ijmtylhtf@naver.com>
2023-05-13 20:09:00 +0900

    [Refactor] #6 - findByChild 함수 생성

--- src/main/java/com/example/domain/usedTime/repository/UsedTimeRepository.java
+++ src/main/java/com/example/domain/usedTime/repository/UsedTimeRepository.java
@@ -1,15 +1,11 @@
 package com.example.domain.usedTime.repository;
 
 import com.example.domain.child.entity.Child;
-import com.example.domain.parent.entity.Parent;
 import com.example.domain.usedTime.entity.UsedTime;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
-import java.util.Optional;
-import java.util.OptionalInt;
 
-public interface UsedTimeRepository extends JpaRepository<UsedTime, Long> {
-    Optional<UsedTime> findByDomainName(String domainName);
+public interface UsedTimeRepository extends JpaRepository<UsedTime,Long> {
     List<UsedTime> findByChild(Child child);
 }
