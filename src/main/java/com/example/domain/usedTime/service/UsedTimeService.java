bfadd0c41073c8cf32282883f458ac2ffbb40778
minjun <ijmtylhtf@naver.com>
2023-05-16 19:36:10 +0900

    [Refactor] #4 - UsedTime insert, update 로직 변경

--- src/main/java/com/example/domain/usedTime/service/UsedTimeService.java
+++ src/main/java/com/example/domain/usedTime/service/UsedTimeService.java
@@ -20,49 +20,85 @@ public class UsedTimeService {
 
     private final ChildRepository childRepository;
 
+    //도메인에 대한 패킷 저장 업데이트
+    public void insertUsedTimeByDomain(Child child, UsedTime existedUsedTime, Long usedTime, String extractedIpAddress, String domainName){
+        System.out.println("UsedTime updated in DB " + domainName);
+        Long firstCapturedTime = existedUsedTime.getFirstCapturedTime();
+        //2번 update
+        Long id = existedUsedTime.getId();
+        int capturedNum = existedUsedTime.getCapturedNum();
+        UsedTime existedUsedTimeForUpdate = usedTimeRepository.findById(id).get();
+        capturedNum +=1;
 
-    public void insertNaverUsedTime(Long userIdx, Long usedTime){
-
-        Child child = childRepository.findById(userIdx).get();
-
-        UsedTime newUsedTime = UsedTime.builder()
+        UsedTime updatedUsedTime = UsedTime.builder()
+                .id(id)
                 .usedTime(usedTime)
+                .firstCapturedTime(firstCapturedTime)
                 .child(child)
-                .domainName("www.naver.com")
-                .ipAddress("223.130")
+                .capturedNum(capturedNum)
+                .domainName(domainName)
+                .ipAddress(extractedIpAddress)
                 .build();
-
-
-        if(usedTimeRepository.findByChild(child).isEmpty()){
-            //Child 없으면 무조건 새로 저장
-            usedTimeRepository.save(newUsedTime);
-        }
-        else{
-            //Child로 조회했을 때,
-            //1. 도메인 네임 다르면 => 무조건 저장
-            //2. 도메인 네임 같은 게 존재하면 => 업데이트
-            int domainCount=0;
-            List<UsedTime> usedTimeWithDomains = usedTimeRepository.findByChild(child);
-            for (UsedTime usedTimeWithDomain : usedTimeWithDomains) {
-                if(usedTimeWithDomain.getDomainName()=="www.naver.com"){
-                    domainCount ++;
-                    //2번 update
-                    Long id = usedTimeWithDomain.getId();
-                    UsedTime usedTimeByNaver = usedTimeRepository.findById(id).get();
-                    UsedTime updatedUsedTime = UsedTime.builder()
-                            .id(id)
-                            .child(child)
-                            .domainName("www.naver.com")
-                            .ipAddress("223.130")
-                            .usedTime(usedTime)
-                            .build();
-                    usedTimeRepository.save(updatedUsedTime);
-                }
-            }
-            if(domainCount==0){
-                //1번 insert
-                usedTimeRepository.save(newUsedTime);
-            }
-        }
+        usedTimeRepository.save(updatedUsedTime);
     }
+
+
+    //해당 도메인에 관련딘 패킷 첫 저장
+    public void insertFirstCaptureTime(Child child, String ipAddress, String domainName, Long firstTimeCapturedTime) {
+
+        UsedTime firstUsedTime = UsedTime.builder()
+                .usedTime(0L)
+                .firstCapturedTime(firstTimeCapturedTime)
+                .child(child)
+                .capturedNum(1)
+                .domainName(domainName)
+                .ipAddress(ipAddress)
+                .build();
+        usedTimeRepository.save(firstUsedTime);
+    }
+
+//    public void insertNaverUsedTime(Long userIdx, Long usedTime){
+//
+//        Child child = childRepository.findById(userIdx).get();
+//
+//        UsedTime newUsedTime = UsedTime.builder()
+//                .usedTime(usedTime)
+//                .child(child)
+//                .domainName("www.naver.com")
+//                .ipAddress("223.130")
+//                .build();
+//
+//
+//        if(usedTimeRepository.findByChild(child).isEmpty()){
+//            //Child 없으면 무조건 새로 저장
+//            usedTimeRepository.save(newUsedTime);
+//        }
+//        else{
+//            //Child로 조회했을 때,
+//            //1. 도메인 네임 다르면 => 무조건 저장
+//            //2. 도메인 네임 같은 게 존재하면 => 업데이트
+//            int domainCount=0;
+//            List<UsedTime> usedTimeWithDomains = usedTimeRepository.findByChild(child);
+//            for (UsedTime usedTimeWithDomain : usedTimeWithDomains) {
+//                if(usedTimeWithDomain.getDomainName()=="www.naver.com"){
+//                    domainCount ++;
+//                    //2번 update
+//                    Long id = usedTimeWithDomain.getId();
+//                    UsedTime usedTimeByNaver = usedTimeRepository.findById(id).get();
+//                    UsedTime updatedUsedTime = UsedTime.builder()
+//                            .id(id)
+//                            .child(child)
+//                            .domainName("www.naver.com")
+//                            .ipAddress("223.130")
+//                            .usedTime(usedTime)
+//                            .build();
+//                    usedTimeRepository.save(updatedUsedTime);
+//                }
+//            }
+//            if(domainCount==0){
+//                //1번 insert
+//                usedTimeRepository.save(newUsedTime);
+//            }
+//        }
+//    }
 }
