75a56a57135c65d5e3c872a86e32e9fdd542c895
minjun <ijmtylhtf@naver.com>
2023-05-23 19:43:44 +0900

    [add] #4 - usedTime 조회 api 생성

--- src/main/java/com/example/domain/usedTime/api/UsedTimeController.java
+++ src/main/java/com/example/domain/usedTime/api/UsedTimeController.java
@@ -1,12 +1,14 @@
 package com.example.domain.usedTime.api;
 
 
+import com.example.domain.usedTime.dto.GetUsedTimeRes;
 import com.example.domain.usedTime.service.UsedTimeService;
 import com.example.global.JPacketCapture;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.concurrent.ExecutionException;
 
 @RestController
@@ -19,6 +21,11 @@ public class UsedTimeController {
     @GetMapping("/capture/{userId}")
     public void capturePacket(@PathVariable(name = "userId") Long userIdx) throws IOException, ExecutionException, InterruptedException {
         jPacketCapture.startCaptureByInterface(userIdx);
+    }
+
+    @GetMapping("/all/{userId}")
+    public List<GetUsedTimeRes> getAllUsedTime(@PathVariable(name = "userId") Long userIdx){
+        return usedTimeService.getAllUsedTime(userIdx);
     }
 
 }
