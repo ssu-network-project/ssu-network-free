514d6842dc627b2c879e83cb16ec5625d94cdecf
minjun <ijmtylhtf@naver.com>
2023-05-29 00:54:32 +0900

    [Add] #22 - 남은 시간 repsonsedto

--- src/main/java/com/example/domain/child/dto/RemainTimeRes.java
+++ src/main/java/com/example/domain/child/dto/RemainTimeRes.java
@@ -1,2 +1,31 @@
-package com.example.domain.child.dto;public class RemainTimeRes {
+package com.example.domain.child.dto;
+
+import com.example.domain.timeGoal.entity.TimeGoal;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+
+/**도메인 별 목표 시간 대비 남은 시간 조회 API의 Response Body**/
+
+/**
+ * 도메인 주소
+ * 남은 시간(분)
+ * 남은 시간(초)
+ */
+@Builder(access = AccessLevel.PRIVATE)
+@Getter
+public class RemainTimeRes {
+
+    String domainName;
+
+    Long remainMinutes;
+
+    Long remainSeconds;
+    public static RemainTimeRes of(TimeGoal timeGoal, Long remainMinutes, Long remainSeconds){
+        return RemainTimeRes.builder()
+                .domainName(timeGoal.getDomainName())
+                .remainMinutes(remainMinutes)
+                .remainSeconds(remainSeconds)
+                .build();
+    }
 }
